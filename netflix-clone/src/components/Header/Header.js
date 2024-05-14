a7cb6aa91cc7759360004d7b7b34a7d963c55578
Samrawit Hailu Tadesse <152161854+samrawit2@users.noreply.github.com>
2024-05-14 23:48:40 +0300

    Commit 3

--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./header.css";
+import NetflixLogo from "../../assets/images/NetflixLogo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const Header = () => {
 	return (
 		<div className="header_outer_container">
 			<div className="header_container">
 				<div className="header_left">
 					<ul>
-						{/*<li><img src ={NetflixLogo} alt= "Netflix Logo" width = "100"/></li>*/}
+						<li>
+							<img src={NetflixLogo} alt="Netflix Logo" width="100" />
+						</li>
 						<li>Netflix</li>
 						<li>Home</li>
 						<li>TVShows</li>
@@ -18,18 +25,18 @@ const Header = () => {
 				</div>
 				<div className="header_right">
 					<ul>
-						{/* <li>
+						<li>
 							<SearchIcon />
 						</li>
 						<li>
-							<NotificationNoneIcon />
+							<NotificationsNoneIcon />
 						</li>
 						<li>
 							<AccountBoxIcon />
 						</li>
 						<li>
 							<ArrowDropDownIcon />
-						</li> */}
+						</li>
 					</ul>
 				</div>
 			</div>
